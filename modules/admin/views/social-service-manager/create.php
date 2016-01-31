bbca371009d2d1786e9dcd575d5bc41b45ebb1e0
Carlos Araujo <carlos943608@hotmail.com>
2016-01-30 18:50:02 -0600

    Eliminar cuentas

---
+++ modules/admin/views/social-service-manager/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\SocialServiceManager */
+
+$this->title = 'Create Social Service Manager';
+$this->params['breadcrumbs'][] = ['label' => 'Social Service Managers', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="social-service-manager-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
