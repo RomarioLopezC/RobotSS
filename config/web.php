a0a51808c1636f5dfbf29b552999370630b95ca5
David Cocom <davcocom@hotmail.com>
2016-02-07 11:27:28 -0600

    Traduccion de mensajes

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'es',
     'modules' => [
         'user' => [
             'class' => 'dektrium\user\Module',
@@ -27,80 +28,80 @@ $config = [
             'class' => 'app\modules\student\Student',
         ],
     ],
-    'components' => [
-        'view' => [
-            'theme' => [
-                'pathMap' => [
-                    '@dektrium/user/views' => '@app/views/user'
+        'components' => [
+            'view' => [
+                'theme' => [
+                    'pathMap' => [
+                        '@dektrium/user/views' => '@app/views/user'
+                    ],
                 ],
             ],
-        ],
-        'authManager' => [
-            'class' => 'yii\rbac\DbManager'
-        ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-        ],
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'LbpHC5bAfg2EcvCffL9donu5j6aYeAen',
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'dektrium\user\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',
-                'username' => 'pruebas.dev4@gmail.com',
-                'password' => 'pruebasdev',
-                'port' => '587',
-                'encryption' => 'tls',
+            'authManager' => [
+                'class' => 'yii\rbac\DbManager'
             ],
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+            'urlManager' => [
+                'enablePrettyUrl' => true,
+                'showScriptName' => false,
+            ],
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => 'LbpHC5bAfg2EcvCffL9donu5j6aYeAen',
+            ],
+            'cache' => [
+                'class' => 'yii\caching\FileCache',
+            ],
+            'user' => [
+                'identityClass' => 'dektrium\user\models\User',
+                'enableAutoLogin' => true,
+            ],
+            'errorHandler' => [
+                'errorAction' => 'site/error',
+            ],
+            'mailer' => [
+                'class' => 'yii\swiftmailer\Mailer',
+                // send all mails to a file by default. You have to set
+                // 'useFileTransport' to false and configure a transport
+                // for the mailer to send real emails.
+                'useFileTransport' => false,
+                'transport' => [
+                    'class' => 'Swift_SmtpTransport',
+                    'host' => 'smtp.gmail.com',
+                    'username' => 'pruebas.dev4@gmail.com',
+                    'password' => 'pruebasdev',
+                    'port' => '587',
+                    'encryption' => 'tls',
                 ],
             ],
+            'log' => [
+                'traceLevel' => YII_DEBUG ? 3 : 0,
+                'targets' => [
+                    [
+                        'class' => 'yii\log\FileTarget',
+                        'levels' => ['error', 'warning'],
+                    ],
+                ],
+            ],
+            'db' => require(__DIR__ . '/db.php'),
         ],
-        'db' => require(__DIR__ . '/db.php'),
-    ],
-    'as access' => [
-        'class' => 'mdm\admin\components\AccessControl',
-        'allowActions' => [
-            'site/*',
-            'user/*',
-            'developer/*',
-            'admin/*',
-            'student/*',
-            'gii/*'
-            //'some-controller/some-action',
-            // The actions listed here will be allowed to everyone including guests.
-            // So, 'admin/*' should not appear here in the production, of course.
-            // But in the earlier stages of your development, you may probably want to
-            // add a lot of actions here until you finally completed setting up rbac,
-            // otherwise you may not even take a first step.
-        ]
-    ],
-    'params' => $params,
-];
+        'as access' => [
+            'class' => 'mdm\admin\components\AccessControl',
+            'allowActions' => [
+                'site/*',
+                'user/*',
+                'developer/*',
+                'admin/*',
+                'student/*',
+                'gii/*'
+                //'some-controller/some-action',
+                // The actions listed here will be allowed to everyone including guests.
+                // So, 'admin/*' should not appear here in the production, of course.
+                // But in the earlier stages of your development, you may probably want to
+                // add a lot of actions here until you finally completed setting up rbac,
+                // otherwise you may not even take a first step.
+            ]
+        ],
+        'params' => $params,
+    ];
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
