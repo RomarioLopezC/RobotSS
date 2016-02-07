8c733462ce2bc58622348b614ec313552f97affd
David Cocom <davcocom@hotmail.com>
2016-02-07 12:33:02 -0600

    Ejemplo migracion roles

--- migrations/m160207_175721_adminRole.php
+++ migrations/m160207_175721_adminRole.php
@@ -7,31 +7,25 @@ class m160207_175721_adminRole extends Migration
 {
     public function up()
     {
-        $this->insert('auth_item',[
-            'name'=>'admin',
-            'type'=>'1',
-            'description'=>'El administrador del sistema para el Servicio social de la UADY',
-            'rule_name'=>null,
-            'data'=>null,
-            'created_at'=>'1453888424',
-            'updated_at'=>'1453888424'
+        $this->insert('auth_item', [
+            'name' => 'admin',
+            'type' => '1',
+            'description' => 'El administrador del sistema para el Servicio social de la UADY',
+            'rule_name' => null,
+            'data' => null,
+            'created_at' => '1453888424',
+            'updated_at' => '1453888424'
         ]);
-        $this->insert('user',[
-            'user_id'=>0,
-            'username'=>'admin',
-            'email'=>'user@correo.com',
-            'password_hash'=>'$2y$10$Wt4jSEPi9nsfK4IaEPOp5expZS.WKI8JDxpAV8hSdpfV0btDUpSGW',
-            'auth_key'=>'iPYS2YokaenQ_-K4itG2y9SIXkQbuB2b',
-            'confirmed_at'=>'1453852050'
+
+        $this->insert('auth_assignment', [
+            'item_name' => 'admin',
+            'user_id' => 10,
+            'created_at' => '1453888440'
         ]);
-        $this->insert('auth_assignment',[
-            'item_name'=>'admin',
-            'user_id'=>0,
-            'created_at'=>'1453888440'
-        ]);
-        $this->insert('auth_item_child',[
-            'parent'=>'admin',
-            'child'=>'/admin/*'
+
+        $this->insert('auth_item_child', [
+            'parent' => 'admin',
+            'child' => '/admin/*'
         ]);
     }
 
